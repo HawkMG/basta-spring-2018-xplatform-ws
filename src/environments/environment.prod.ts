b13e9de1357519ca6e63c2e6442ecfb685c3fe5e
Christian Liebel <christian.liebel@thinktecture.com>
2018-02-14 15:51:16 +0100

    Adding demo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://0daba550.ngrok.io/api/',
+    push: {
+        publicKey: 'BBh_zx5aEnlMyrM8W8anuyxx2ibkb9cUxZclHHDHuBd3uX7PNp-minttLaWe0jpOiHvNfUHXD1rUXTfYf87URlE'
+    }
+};
