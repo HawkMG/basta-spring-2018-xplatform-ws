d3ec85e0a4be6269452784ad6707a0e0a5744d91
Christian Liebel <christian.liebel@thinktecture.com>
2018-02-15 10:16:41 +0100

    Share services

--- src/modules/shared/services/index.ts
+++ src/modules/shared/services/index.ts
@@ -1,15 +1,16 @@
-import {FeatureService} from './feature.service';
-import {TodoService} from './base/todo.service';
-import {TodoServiceRef} from './todo.service.ref';
-import {ApiService} from './base/api.service';
 import {ApiServiceRef} from './api.service.ref';
-import {SyncService} from './base/sync.service';
-import {SyncServiceRef} from './sync.service.ref';
-import {DatabaseService} from './base/database.service';
-import {DatabaseServiceRef} from './database.service.ref';
-import {NotificationService} from './notification.service';
-import {ShareService} from './share.service';
 import {AppStateService} from './appState.service';
+import {ApiService} from './base/api.service';
+import {DatabaseService} from './base/database.service';
+import {ShareService} from './base/share.service';
+import {SyncService} from './base/sync.service';
+import {TodoService} from './base/todo.service';
+import {DatabaseServiceRef} from './database.service.ref';
+import {FeatureService} from './feature.service';
+import {NotificationService} from './notification.service';
+import {ShareServiceFactoryDeps, ShareServiceFactory} from './share/share.service.factory';
+import {SyncServiceRef} from './sync.service.ref';
+import {TodoServiceRef} from './todo.service.ref';
 
 export const SHARED_SERVICES = [
     FeatureService,
@@ -17,7 +18,7 @@ export const SHARED_SERVICES = [
     { provide: ApiService, useClass: ApiServiceRef },
     { provide: SyncService, useClass: SyncServiceRef },
     { provide: DatabaseService, useClass: DatabaseServiceRef },
+    { provide: ShareService, useFactory: ShareServiceFactory, deps: ShareServiceFactoryDeps },
     NotificationService,
-    ShareService,
     AppStateService,
 ];
